84f5cc45ff8dc4a2fdbd215c9dfbf49cc827da7a
Philip Lages <psilvaelages@gmail.com>
2023-01-17 11:57:13 -0300

    feat: creates first migration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,17 @@
 import Fastify from "fastify";
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient();
+
+app.register(cors);
+
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
 
 const port = 3333;
 
