db5545d7bc2730dff5bbee915d90a65935a6ad99
Philip Lages <psilvaelages@gmail.com>
2023-01-17 19:07:05 -0300

    feat: creates loading component styles statusbar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import {
+  useFonts, 
+  Inter_400Regular, 
+  Inter_600SemiBold, 
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter';
+
+import Loading from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded) {
+    return ( <Loading /> )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.container }>
+      <Text style={ styles.text }>Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
     </View>
   );
 }
@@ -13,8 +30,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'inter_800ExtraBold',
+    color: '#FFF'
+  }
 });
