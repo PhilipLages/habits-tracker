b3d4b0f11d9b7aeaea2e83bc1cb8959d4b1ffb2b
Philip Lages <psilvaelages@gmail.com>
2023-01-17 12:55:27 -0300

    styles: creates habits divs styles

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -2,6 +2,8 @@ import HabitProps from "./interfaces/HabitProps";
 
 export default function Habit({ completed }: HabitProps) {
   return (
-    <div>{completed}</div>
+    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
+      {completed}
+    </div>
   )
 }
