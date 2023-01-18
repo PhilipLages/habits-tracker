b6aa76b4cf63a5cd4bc2494beca881745f970142
Philip Lages <psilvaelages@gmail.com>
2023-01-18 11:13:14 -0300

    feat: creates get days

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,11 +4,11 @@ import { z } from 'zod';
 import { prisma } from './lib/prisma';
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/habits', async (req, res ) => {
+  app.post('/habits', async (req, _res ) => {
     const createHabitBody = z.object({
       title: z.string(),
       weekDays: z.array(z.number().min(0).max(6))
-    })
+    });
 
     const { title, weekDays } = createHabitBody.parse(req.body);
 
@@ -26,7 +26,48 @@ export async function appRoutes(app: FastifyInstance) {
           })
         }
       }
-    })
+    });
   });
-  
+
+  app.get('/day', async (req, _res) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    });
+
+    const { date } = getDayParams.parse(req.query);
+
+    const parsedDate = dayjs(date).startOf('day');
+    const weekDay = parsedDate.get('day');
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          }
+        }
+      }
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      }
+    });
+
+    const completedHabits = day?.dayHabits.map(dayHabit => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
+  });
 };
