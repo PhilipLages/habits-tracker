2de40712de5a094a9423d376a6d1d6dbe5c41900
Philip Lages <psilvaelages@gmail.com>
2023-01-18 09:46:17 -0300

    fix: changes font family name

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -35,7 +35,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontFamily: 'inter_800ExtraBold',
-    color: '#FFF'
+    color: '#FFF',
+    fontFamily: 'Inter_800ExtraBold'
   }
 });
