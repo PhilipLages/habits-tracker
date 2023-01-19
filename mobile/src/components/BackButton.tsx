f7ed374bf0a4063e5768ac0c18d785519c3ee944
Philip Lages <psilvaelages@gmail.com>
2023-01-19 13:33:26 -0300

    feat: creates habit input

--- mobile/src/components/BackButton.tsx
+++ mobile/src/components/BackButton.tsx
@@ -1,10 +1,16 @@
 import { TouchableOpacity } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
+import { useNavigation } from '@react-navigation/native';
 
 export function BackButton() {
+  const { goBack } = useNavigation()
+
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+    activeOpacity={0.7}
+    onPress={goBack}
+    >
       <Feather
         name='arrow-left'
         size={32}
