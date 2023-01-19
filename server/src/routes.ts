7e6224a30430adbd292171177c24e7de411ccb7b
Philip Lages <psilvaelages@gmail.com>
2023-01-19 10:55:00 -0300

    feat: creates route to toggle a habit as completed or uncompleted

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -52,7 +52,7 @@ export async function appRoutes(app: FastifyInstance) {
       }
     });
 
-    const day = await prisma.day.findUnique({
+    const day = await prisma.day.findFirst({
       where: {
         date: parsedDate.toDate(),
       },
@@ -70,4 +70,57 @@ export async function appRoutes(app: FastifyInstance) {
       completedHabits
     }
   });
+
+  app.patch('/habits/:id/toggle', async (req, _res) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toggleHabitParams.parse(req.params);
+
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      }
+    });
+
+    if(!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    });
+
+    if(dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      // completar hábito
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      });
+    }
+  });
+
+  app.get('/summary', async () => {
+    
+  })
 };
