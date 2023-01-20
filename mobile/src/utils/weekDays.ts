2d410462688ce082ef9d205a18e40702bde5eef3
Philip Lages <psilvaelages@gmail.com>
2023-01-20 15:13:43 -0300

    feat: creates function to create new habit with the api

--- mobile/src/utils/weekDays.ts
+++ mobile/src/utils/weekDays.ts
@@ -1,1 +1,11 @@
 export const weekDays = [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+export const availableWeekDays = [
+  'Domingo', 
+  'Segunda-feira', 
+  'Terça-feira', 
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado'
+];
