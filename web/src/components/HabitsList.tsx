1b009e8b5fecb5c5ebbd8c92138da6abaaee1f5e
Philip Lages <psilvaelages@gmail.com>
2023-01-21 13:06:24 -0300

    feat: creates logic to check and uncheck habits, with request from api

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,4 +1,5 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
+import dayjs from "dayjs";
 import { Check } from "phosphor-react";
 import { useEffect, useState } from "react";
 import { api } from "../lib/axios";
@@ -13,26 +14,55 @@ export function HabitsList({ date }: HabitsListProps) {
       params: {
         date: date.toISOString(),
       }
-    }).then(({ data  }) => data)
-  }, [])
+    }).then(({ data  }) => {
+      setHabitsInfo(data);
+    })
+  }, []);
+
+  const handleToggleHabit = async (habitId: string) => {
+    const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId);
+
+    await api.patch(`habits/${habitId}/toggle`);
+
+    let completedHabits: string[] = [];
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId);
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId]
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    })
+  };
+
+  const isDatePassed = dayjs(date).endOf('day').isBefore(new Date());
 
   return (
     <div className='mt-6 flex flex-col gap-3'>
-      <Checkbox.Root
-        className='flex items-center gap-3 group'
-      >
-        <div 
-          className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+      { habitsInfo?.possibleHabits.map(habit => (
+        <Checkbox.Root
+          key={ habit.id }
+          onCheckedChange={ () => handleToggleHabit(habit.id) }
+          checked={ habitsInfo.completedHabits.includes(habit.id) }
+          disabled={ isDatePassed }
+          className='flex items-center gap-3 group'
         >
-          <Checkbox.Indicator>
-            <Check size={20} className='text-white' />
-          </Checkbox.Indicator>
-        </div>
+          <div 
+            className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+          >
+            <Checkbox.Indicator>
+              <Check size={20} className='text-white' />
+            </Checkbox.Indicator>
+          </div>
 
-        <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
-          Beber 2l de agua
-        </span>
-      </Checkbox.Root>
+          <span className='font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+            { habit.title }
+          </span>
+        </Checkbox.Root>
+      )) }
     </div>
   )
 }
