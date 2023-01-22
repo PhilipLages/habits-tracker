ca59603a2865080284974062006ad0d183670228
Philip Lages <psilvaelages@gmail.com>
2023-01-22 10:55:52 -0300

    style: animates checkbox and progressbar from habits

--- mobile/src/components/Checkbox.tsx
+++ mobile/src/components/Checkbox.tsx
@@ -1,4 +1,6 @@
 import { View, TouchableOpacity, Text } from "react-native";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
+
 import { Feather } from '@expo/vector-icons';
 import colors from "tailwindcss/colors";
 import CheckboxProps from "./interfaces/checkboxProps";
@@ -10,20 +12,20 @@ export function Checkbox({ checked = false, title, ...rest }: CheckboxProps) {
       className="flex-row mb-2 items-center"
       { ...rest }
     >
-      {
-        checked 
-        ?
-        <View
-          className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
-        >
-          <Feather
-            name="check"
-            size={20}
-            color={colors.white}
-          />
-        </View>
-        :
-        <View className="h-8 w-8 bg-zinc-900 rounded-lg"/>
+      { checked  ? (
+          <Animated.View
+            className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center"
+            entering={ZoomIn}
+            exiting={ZoomOut}
+          >
+            <Feather
+              name="check"
+              size={20}
+              color={colors.white}
+            />
+          </Animated.View>
+
+        ) :  <View className="h-8 w-8 bg-zinc-900 rounded-lg"/>
       }
 
       <Text className="text-white text-base ml-3 font-semibold">
